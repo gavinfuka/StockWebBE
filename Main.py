1001d74c394954624570caaa9213263284bbf145
Gavin Fu <gavin.fu@mobinology.com>
2020-08-29 14:31:44 +0800

    Main

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 
 from app import app as Backend
 if __name__ ==  "__main__":
-    Backend.run( host="0.0.0.0", port="5000",debug=True)
+    Backend.run( host="0.0.0.0", port="4000",debug=True)
     Backend.config['JSON_AS_ASCII'] = False
 
